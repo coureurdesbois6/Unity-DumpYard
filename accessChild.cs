80e704c064705676c79236853f0fe9d5862ee5b4
coureurdesbois6 <46076235+coureurdesbois6@users.noreply.github.com>
2018-12-23 02:59:28 +0300

    Update accessChild.cs

--- accessChild.cs
+++ accessChild.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class positionAll : MonoBehaviour
-{
+public class positionAll : MonoBehaviour {
     public Vector3 position;
-    void Start()
-    {
-        for (int i = 0; i <= transform.childCount-1; i++) {
+
+    void Start() {
+        for (int i = 0; i <= transform.childCount - 1; i++) {
             transform.GetChild(i).transform.position = position;
         }
     }
